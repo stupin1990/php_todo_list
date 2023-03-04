c093a863a94b2baf59b56f48dde468951b7c0e27
stupin1990 <stupin1990@gmail.com>
2023-03-04 23:42:20 +0300

    slash fix

--- autoloader.php
+++ autoloader.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    $path = __DIR__ . '/' . $class . '.php';
+    $path = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
 
     if (!file_exists($path)) {
         echo '404 Not Found';
