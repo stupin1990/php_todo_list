efccb8d04d1b38cf535840324d1d4717d63ea3a9
stupin1990 <stupin1990@gmail.com>
2023-03-05 16:26:04 +0300

    public update

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/autoloader.php';
+require_once __DIR__ . '/../autoloader.php';
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path = $path == '/' ? 'App' : trim($path, '/');
